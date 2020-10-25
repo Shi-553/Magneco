b72a16499aa9396dd84df04cded2a243269a841a
shi <sibatamaru@gmail.com>
2020-10-25 14:41:19 +0900

    judge.cppの追加
    当たり判定するファイルです。

---
+++ HEW2/judge.cpp
@@ -0,0 +1,13 @@
+#include "judge.h"
+void InitJudge(){
+
+}
+void UpdateJudge(){
+
+}
+void DrawJudge(){
+
+}
+void UninitJudge(){
+
+}
---
+++ HEW2/judge.h
@@ -0,0 +1,10 @@
+#ifndef _JUDGE_H
+#define _JUDGE_H
+
+void InitJudge();
+void UpdateJudge();
+void DrawJudge();
+void UninitJudge();
+
+
+#endif // !_JUDGE_H
