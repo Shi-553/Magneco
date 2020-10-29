5d32ece66b31dbc0d9affacabebfcebdff31c29b
momonga2525 <arst.0525@gmail.com>
2020-10-30 01:55:41 +0900

    もしも足りなかったり間違えていたら連絡ください。

--- HEW2/Input.cpp
+++ HEW2/Input.cpp
@@ -10,9 +10,29 @@ void InitInput() {
 }
 
 void UninitInput(){
+
 }
 
 void UpdateInput() {
+
+	// プレイヤーの移動（if文の中にplayerの移動書きます）
+	if (PressInputLogger(MYVK_UP)) {
+		
+	}
+	if (PressInputLogger(MYVK_DOWN)) {
+
+	}
+	if (PressInputLogger(MYVK_LEFT)) {
+
+	}
+	if (PressInputLogger(MYVK_RIGHT)) {
+
+	}
+
+	// プレイヤーの決定（現時点ではpressにしてます）
+	if (PressInputLogger(MYVK_ENTER)) {
+
+	}
 
 }
 
--- HEW2/Input.h
+++ HEW2/Input.h
@@ -1,1 +1,15 @@
-#pragma once
+// -------------------------------------------------------------------------
+//	“ü—Í‚ğ‚Æ‚éˆ—[Input.h]
+// 
+//						Author:Kanna Noda
+// -------------------------------------------------------------------------
+#ifndef _INPUT_H
+#define _INPUT_H
+
+void InitInput();
+void UninitInput();
+void UpdateInput();
+void DrawInput();
+
+#endif // !_INPUT_H
+
