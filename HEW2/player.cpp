f2c52ce5f1a2e8fbbf4b47f481071f6d002947e0
araiyouta <tgam5ajp@i.softbank.jp>
2020-11-01 16:42:10 +0900

    Move

--- HEW2/player.cpp
+++ HEW2/player.cpp
@@ -3,22 +3,21 @@
 #include "sprite.h"
 #include "flyingObject.h"
 #include "myd3d.h"
-#include <math.h>
 #include <list>
 #include "player.h"
 #include "game.h"
 
 typedef struct Player_tag{
 	D3DXVECTOR2 position;
-	D3DXVECTOR2 direction;
-	std::list < FlyingObject > flyingObject;
+	std::list < FlyingObject > flyingObjectList;
 }Player;
 
 static int TextureId = TEXTURE_INVALID_ID;
-
+static Player player;
 
 void InitPlayer(){
-
+	player.position = D3DXVECTOR2(3, 3);
+	player.flyingObjectList.clear();
 }
 
 void UninitPlayer(){
@@ -43,21 +42,21 @@ void RotateRightPlayer(){
 }
 
 void MoveUpPlayer(){
-
+	player.position.y += -1.0f;
 }
 
 void MoveDownPlayer(){
-
+	player.position.y += 1.0f;
 }
 
 void MoveLeftPlayer(){
-
+	player.position.x += -1.0f;
 }
 
 void MoveRightPlayer(){
-
+	player.position.x += 1.0f;
 }
 
-void BlockDecision(FlyingObject flyngobject) {
+void BlockDecision(FlyingObject flyngObject) {
 
 }
