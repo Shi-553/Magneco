1e7118df6c29eede34bd48fdcfc8929f2ee2a266
学校用 <ren02157003nao@gmail.com>
2020-11-01 13:52:17 +0900

    マップつくりました

--- HEW2/map.cpp
+++ HEW2/map.cpp
@@ -1,20 +1,22 @@
 #include "map.h"
+#include "myd3d.h"
 #include "texture.h"
 
 
 static MapType MapChipList[MAPCHIP_WIDTH][MAPCHIP_HEIGHT]
 {
-	{},
-	{},
-	{},
-	{},
-	{},
-	{},
-	{},
-	{},
-	{},
-	{},
+	{WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL},
+	{WALL, NONE, NONE, NONE, NONE, NONE, NONE, NONE, NONE, WALL},
+	{WALL, NONE, NONE, NONE, NONE, NONE, NONE, NONE, NONE, WALL},
+	{WALL, NONE, NONE, NONE, NONE, NONE, GOAL, NONE, NONE, WALL},
+	{WALL, NONE, NONE, NONE, NONE, NONE, NONE, NONE, NONE, WALL},
+	{WALL, NONE, NONE, NONE, ROCK, NONE, NONE, NONE, NONE, WALL},
+	{WALL, NONE, NONE, NONE, NONE, NONE, NONE, NONE, NONE, WALL},
+	{WALL, NONE,BLOCK, NONE, NONE, NONE, NONE, NONE, NONE, WALL},
+    {WALL, NONE, NONE, NONE, NONE, NONE, NONE, NONE, NONE, WALL},
+	{WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL},
 };
+
 
 static int TextureId = TEXTURE_INVALID_ID;
 
@@ -26,7 +28,7 @@ void InitMap(void)
 
 void UninitMap(void)
 {
-
+	//Texture_Release(&TextureId, 1);
 }
 
 void UpdateMap(void)
