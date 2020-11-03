5601f1f44b5eea4c4efdbd588e8138e69afeb62f
shi <sibatamaru@gmail.com>
2020-11-03 15:13:36 +0900

    flyingObjectの速度を調整

--- HEW2/flyingObject.cpp
+++ HEW2/flyingObject.cpp
@@ -9,12 +9,14 @@
 
 static std::list<FlyingObject> flyingObjects;
 static int flyingObjectTextureId = TEXTURE_INVALID_ID;
+static int frame = 0;
 
 std::list<FlyingObject>* GetFlyingObjects(){
 	return &flyingObjects;
 }
 
 void InitFlyingObject(){
+	frame = 0;
 	flyingObjectTextureId = ReserveTextureLoadFile("texture/fade.png");
 }
 void UninitFlyingObject(){
@@ -27,7 +29,18 @@ void DrawFlyingObject(){
 	}
 }
 void UpdateFlyingObject(){
-	for (auto itr = flyingObjects.begin(); itr != flyingObjects.end(); itr++) {
-		itr->pos += itr->dir;
+	frame++;
+	if (frame > 100) {
+		frame = 0;
+
+		for (auto itr = flyingObjects.begin(); itr != flyingObjects.end();) {
+			itr->pos += itr->dir;
+			if (itr->pos.x > 10 || itr->pos.x < -10 || itr->pos.y>10 || itr->pos.y < 10) {
+				itr = flyingObjects.erase(itr);
+			}
+			else {
+				itr++;
+			}
+		}
 	}
 }
