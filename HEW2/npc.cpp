2c550c90948f572f56ad6c8c429f486f69df1089
momonga2525 <arst.0525@gmail.com>
2020-11-05 16:47:30 +0900

    npc.cpp,npc.hを変更しました。 npc.pngを追加しました。 npc.hに構造体を移動しました。 npc.cppにnpcを描画する処理を追加しました。

--- HEW2/npc.cpp
+++ HEW2/npc.cpp
@@ -2,22 +2,21 @@
 #include "texture.h"
 #include "sprite.h"
 #include "myd3d.h"
+#include "gameSrite.h"
 #include "npc.h"
 #include <d3dx9.h>
 
-typedef struct NPC_tag{
-	D3DXVECTOR2 NPCPosition;
-}NPC;
-
 
 static int NPCTextureId = TEXTURE_INVALID_ID;
+static NPC npc;
 
 void InitNPC(){
-
+	NPCTextureId = ReserveTextureLoadFile("texture/npc.png");
+	npc.NPCPosition = D3DXVECTOR2(2, 7);
 }
 
 void UninitNPC() {
-
+	ReleaseTexture(NPCTextureId);
 }
 
 void UpdateNPC() {
@@ -25,5 +24,8 @@ void UpdateNPC() {
 }
 
 void DrawNPC() {
+	D3DXVECTOR2 intposition;
+	intposition = D3DXVECTOR2((int)npc.NPCPosition.x, (int)npc.NPCPosition.y);
+	DrawGameSprite(NPCTextureId, intposition, 30);
 
 }
--- HEW2/npc.h
+++ HEW2/npc.h
@@ -1,6 +1,10 @@
 #ifndef _NPC_H
 #define _NPC_H
 
+typedef struct NPC_tag {
+	D3DXVECTOR2 NPCPosition;
+}NPC;
+
 
 void InitNPC();
 void UpdateNPC();
