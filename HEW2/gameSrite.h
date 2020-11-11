b738c3f48492bd3e3ee1191ce930115f02e69293
学校用 <ren02157003nao@gmail.com>
2020-11-11 17:29:01 +0900

    グリッドを付けました

--- HEW2/gameSrite.h
+++ HEW2/gameSrite.h
@@ -1,4 +1,14 @@
 #pragma once
 #include "myd3d.h"
+#include "map.h"
+#include "config.h"
+
+#define GAME_SPRITE_WHIDTH (50)
+#define GAME_SPRITE_HEIGHT (50)
+
+
+#define GAME_SPRITE_ZERO_X (SCREEN_WIDTH  /2 - (GAME_SPRITE_WHIDTH*MAPCHIP_WIDTH ) /2)
+#define GAME_SPRITE_ZERO_Y (SCREEN_HEIGHT /2 - (GAME_SPRITE_HEIGHT*MAPCHIP_HEIGHT) /2)
+
 
 void DrawGameSprite(int textureId, D3DXVECTOR2 pos, float z);
