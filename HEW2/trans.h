c50165f910e2820e2360c82c1c9626858354ec4d
momonga2525 <arst.0525@gmail.com>
2020-11-11 17:11:55 +0900

    移動用の作りました

---
+++ HEW2/trans.h
@@ -0,0 +1,23 @@
+// --------------------------------------------------------------
+//							[trans.h]
+// 
+//							Author:Kanna Noda
+//----------------------------------------------------------------
+
+#include <d3dx9.h>
+
+typedef struct INTVECTOR2 {
+	int x;
+	int y;
+};
+
+typedef struct TRANS {
+	D3DXVECTOR2 pos;
+	INTVECTOR2 intPos;
+	INTVECTOR2 intLastPos;
+};
+
+void MoveTrans(TRANS* trans, D3DXVECTOR2 addPos);
+void MoveTransX(TRANS* trans, float x);
+void MoveTransY(TRANS* trans, float y);
+
