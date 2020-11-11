fdce093b654650883f5e7868ae2641c7151bc4af
shi <sibatamaru@gmail.com>
2020-11-11 23:35:26 +0900

    演算子のオーバーライドをたくさん追加、 posの更新後にUpdateを呼ぶ形に変更

--- HEW2/trans.cpp
+++ HEW2/trans.cpp
@@ -6,106 +6,48 @@
 
 #include "trans.h"
 
-void InitTrans(TRANS* trans, D3DXVECTOR2 pos) {
-	trans->pos = pos;
 
-	trans->intPos.x = (int)trans->pos.x;
-	trans->intPos.y = (int)trans->pos.y;
-
-	trans->intLastPos.x = trans->intPos.x;
-	trans->intLastPos.y = trans->intPos.y;
-
-
-}
-
-
-void ChangeTrans(TRANS* trans, D3DXVECTOR2 pos) {
-	trans->pos = pos;
-
-	INTVECTOR2 compare;
+void UpdateTrans(TRANS* trans) {
 	// 更新した位置を保管
+	INTVECTOR2 compare;
 	compare.x = (int)trans->pos.x;
 	compare.y = (int)trans->pos.y;
 
 	// 更新した位置と元の位置を比較
-	if (compare.x == trans->intPos.x && compare.y == trans->intPos.y) {
+	if (compare == trans->intPos) {
 		return;
 	}
 
-	trans->intLastPos.x = trans->intPos.x;
-	trans->intLastPos.y = trans->intPos.y;
+	trans->intLastPos = trans->intPos;
 
 	// intPosに現在の位置を入れる
-	trans->intPos.x = compare.x;
-	trans->intPos.y = compare.y;
-
+	trans->intPos = compare;
 }
-
-void MoveTrans(TRANS* trans, D3DXVECTOR2 addPos) {
-
-	// 位置にaddPos分追加する
-	trans->pos += addPos;
-
-	INTVECTOR2 compare;
+void UpdateTransX(TRANS* trans) {
 	// 更新した位置を保管
-	compare.x = (int)trans->pos.x;
-	compare.y = (int)trans->pos.y;
+	int x = (int)trans->pos.x;
 
 	// 更新した位置と元の位置を比較
-	if (compare.x == trans->intPos.x && compare.y == trans->intPos.y) {
-		return;
-	}
-
-	trans->intLastPos.x = trans->intPos.x;
-	trans->intLastPos.y = trans->intPos.y;
-
-	// intPosに現在の位置を入れる
-	trans->intPos.x = compare.x;
-	trans->intPos.y = compare.y;
-
-
-}
-
-void MoveTransX(TRANS* trans, float x) {
-	INTVECTOR2 compare;
-
-	// 位置にaddPos分追加する
-	trans->pos.x += x;
-
-	// 更新した位置を保管
-	compare.x = (int)trans->pos.x;
-
-	// 更新した位置と元の位置を比較
-	if (compare.x == trans->intPos.x) {
+	if (x == trans->intPos.x) {
 		return;
 	}
 
 	trans->intLastPos.x = trans->intPos.x;
 
 	// intPosに現在の位置を入れる
-	trans->intPos.x = compare.x;
-
-
+	trans->intPos.x = x;
 }
-
-void MoveTransY(TRANS* trans, float y) {
-	INTVECTOR2 compare;
-
-	// 位置にaddPos分追加する
-	trans->pos.y += y;
-
+void UpdateTransY(TRANS* trans){
 	// 更新した位置を保管
-	compare.y = (int)trans->pos.y;
+	int y = (int)trans->pos.y;
 
 	// 更新した位置と元の位置を比較
-	if (compare.y == trans->intPos.y) {
+	if (y == trans->intPos.y) {
 		return;
 	}
 
 	trans->intLastPos.y = trans->intPos.y;
 
 	// intPosに現在の位置を入れる
-	trans->intPos.y = compare.y;
-
-
+	trans->intPos.y = y;
 }
--- HEW2/trans.h
+++ HEW2/trans.h
@@ -9,16 +9,64 @@
 typedef struct INTVECTOR2 {
 	int x;
 	int y;
+
+	bool operator==(const INTVECTOR2& b)const {
+		return x == b.x && y == b.y;
+	}
+	INTVECTOR2& operator = (const INTVECTOR2& b) {
+		x = b.x;
+		y = b.y;
+		return *this;
+	}
+	const INTVECTOR2 operator + (const INTVECTOR2& b) const
+	{
+		return { x + b.x, y + b.y };
+	}
+	const INTVECTOR2 operator - (const INTVECTOR2& b) const
+	{
+		return { x - b.x, y - b.y };
+	}
+	const INTVECTOR2 operator * (const INTVECTOR2& b) const
+	{
+		return { x * b.x, y * b.y };
+	}
+	const INTVECTOR2 operator / (const INTVECTOR2& b) const
+	{
+		return { x / b.x, y / b.y };
+	}
+	INTVECTOR2& operator += (const INTVECTOR2& b)
+	{
+		x += b.x;
+		y += b.y; 
+		return *this;
+	}
+	INTVECTOR2& operator -= (const INTVECTOR2& b)
+	{
+		x -= b.x;
+		y -= b.y; 
+		return *this;
+	}
+	INTVECTOR2& operator *= (const INTVECTOR2& b)
+	{
+		x *= b.x;
+		y *= b.y; 
+		return *this;
+	}
+	INTVECTOR2& operator /= (const INTVECTOR2& b)
+	{
+		x /= b.x;
+		y /= b.y; 
+		return *this;
+	}
 };
+
 
 typedef struct TRANS {
 	D3DXVECTOR2 pos;
 	INTVECTOR2 intPos;
 	INTVECTOR2 intLastPos;
 };
-void InitTrans(TRANS* trans, D3DXVECTOR2 pos);
-void ChangeTrans(TRANS* trans, D3DXVECTOR2 pos);
-void MoveTrans(TRANS* trans, D3DXVECTOR2 addPos);
-void MoveTransX(TRANS* trans, float x);
-void MoveTransY(TRANS* trans, float y);
 
+void UpdateTrans(TRANS* trans);
+void UpdateTransX(TRANS* trans);
+void UpdateTransY(TRANS* trans);
