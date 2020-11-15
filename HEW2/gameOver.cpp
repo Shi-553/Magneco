dc8b9dad24924ed915b1455b5e6e0caf0babe7e2
momonga2525 <arst.0525@gmail.com>
2020-11-15 16:42:53 +0900

    ゲームオーバー時に使用する画像三枚追加しました。 （ボタン＊２、ロゴ＊１） ゲームオーバー時の処理を追加しました。

--- HEW2/gameOver.cpp
+++ HEW2/gameOver.cpp
@@ -1,24 +1,79 @@
-#include "gameOver.h"
+
+#include "config.h"
 #include "texture.h"
 #include "sprite.h"
-#include "InputLogger.h"
+#include "gameOver.h"
 #include "sceneManager.h"
-#include "config.h"
+#include "InputLogger.h"
+#include "selectButton.h"
 
-int gameOverTexture;
+#define GAME_OVER_BUTTON_WIDTH 256
+#define GAME_OVER_BUTTON_HEIGHT 128
+
+#define GAME_OVER_LOGO_WIDTH 640
+#define GAME_OVER_LOGO_HEIGHT 240
+
+static int gameOverTexture;
+static int gameOverTexture_2;
+
+static Button restartButton, backtitleButton;
+
 void InitGameOver() {
+	InitSelectButton();
+
+	auto buttonCenter = D3DXVECTOR2(SCREEN_WIDTH / 2 - GAME_OVER_BUTTON_WIDTH / 2, SCREEN_HEIGHT / 2 - GAME_OVER_BUTTON_HEIGHT / 2);
+
+	restartButton.pos = buttonCenter;
+	restartButton.size = D3DXVECTOR2(GAME_OVER_BUTTON_WIDTH, GAME_OVER_BUTTON_HEIGHT);
+	restartButton.frameColor = D3DCOLOR_RGBA(0, 0, 0, 255);
+	restartButton.textureId = ReserveTextureLoadFile("texture/restartButton.png");
+
+	restartButton.pressedCallback = []() {
+		GoNextScene(GameScene);
+	};
+
+	backtitleButton.pos = buttonCenter + D3DXVECTOR2(0, 200);
+	backtitleButton.size = D3DXVECTOR2(GAME_OVER_BUTTON_WIDTH, GAME_OVER_BUTTON_HEIGHT);
+	backtitleButton.frameColor = D3DCOLOR_RGBA(255, 0, 0, 255);
+	backtitleButton.textureId = ReserveTextureLoadFile("texture/backtitleButton.png");
+
+	backtitleButton.pressedCallback = []() {
+		GoNextScene(GameStartScene);
+	};
+
+	AddButton(&restartButton);
+	AddButton(&backtitleButton);
+
 	gameOverTexture = ReserveTextureLoadFile("texture/îwåiÇP.png");
+
+	gameOverTexture_2 = ReserveTextureLoadFile("texture/gameover_640Å~240.png");
+
 	LoadTexture();
 }
+
 void UninitGameOver() {
+	UninitSelectButton();
+
 	ReleaseTexture(gameOverTexture);
+	ReleaseTexture(gameOverTexture_2);
+	ReleaseTexture(restartButton.textureId);
+	ReleaseTexture(backtitleButton.textureId);
+
 }
 void DrawGameOver() {
 	DrawSprite(gameOverTexture, { 0,0 }, 10, { SCREEN_WIDTH,SCREEN_HEIGHT }, { 0,0 }, { SCREEN_WIDTH,SCREEN_HEIGHT });
-
+	DrawSprite(gameOverTexture_2, { 325,0 }, 10, { GAME_OVER_LOGO_WIDTH,GAME_OVER_LOGO_HEIGHT }, { 0,0 }, { GAME_OVER_LOGO_WIDTH,GAME_OVER_LOGO_HEIGHT });
+	DrawSelectButton();
 }
+
 void UpdateGameOver() {
-	if (ReleaseInputLogger(MYVK_ENTER)) {
-		GoNextScene(GameStartScene);
+	if (TriggerInputLogger(MYVK_ENTER)) {
+		EnterSelectButton();
+	}
+	if (TriggerInputLogger(MYVK_UP)) {
+		BackSelectButton();
+	}
+	if (TriggerInputLogger(MYVK_DOWN)) {
+		ForwardSelectButton();
 	}
 }
