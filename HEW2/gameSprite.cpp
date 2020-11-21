c17da2df13ffc310ec463aeebeb02f69583af55c
竹内廉 <ren02157003nao@gmail.com>
2020-11-21 13:37:19 +0900

    壁を付けました

--- HEW2/gameSprite.cpp
+++ HEW2/gameSprite.cpp
@@ -5,6 +5,16 @@
 
 
 
+void DrawGameSprite(int textureId, D3DXVECTOR2 pos, float z, D3DXVECTOR2 tPos, D3DXVECTOR2 tSize) {
+	auto gameSpritePos = D3DXVECTOR2(
+		GAME_SPRITE_ZERO_X + (pos.x * GAME_SPRITE_WHIDTH),
+		GAME_SPRITE_ZERO_Y + (pos.y * GAME_SPRITE_HEIGHT));
+
+	auto gameSpriteSize = D3DXVECTOR2(GAME_SPRITE_WHIDTH, GAME_SPRITE_HEIGHT);
+
+	DrawSprite(textureId, gameSpritePos, z, gameSpriteSize, tPos, tSize);
+}
+
 void DrawGameSprite(int textureId, D3DXVECTOR2 pos, float z) {
 	auto gameSpritePos = D3DXVECTOR2(
 		GAME_SPRITE_ZERO_X + (pos.x * GAME_SPRITE_WHIDTH),
