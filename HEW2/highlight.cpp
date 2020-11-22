257fe56e55368cd3a5198f97823692c8bb81aca0
shi <sibatamaru@gmail.com>
2020-11-22 17:14:31 +0900

    ハイライト追加

---
+++ HEW2/highlight.cpp
@@ -0,0 +1,19 @@
+
+#include "highlight.h"
+#include "texture.h"
+#include "sprite.h"
+
+static int highlightRoundTexture = TEXTURE_INVALID_ID;
+
+void InitHighlight() {
+
+}
+void UninitHighlight() {
+
+}
+void DrawHighlight(D3DXVECTOR2 pos) {
+
+}
+void DrawFrontBlack() {
+
+}
---
+++ HEW2/highlight.h
@@ -0,0 +1,6 @@
+#pragma once
+
+void InitHighlight();
+void UninitHighlight();
+void DrawHighlight(D3DXVECTOR2 pos);
+void DrawFrontBlack();
