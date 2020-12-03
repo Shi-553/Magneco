5edcf49a4c132b6d8f59960a6b4164ea6439926e
shi <sibatamaru@gmail.com>
2020-12-03 15:39:17 +0900

    引数を増やしていろいろできるようにしました

--- HEW2/highlight.cpp
+++ HEW2/highlight.cpp
@@ -12,7 +12,7 @@ struct Vertex2D{
 #define PI (3.141519)
 
 
-void DrawCircle(D3DXVECTOR2 pos, float size, int count);
+void DrawCircle(D3DXVECTOR2 pos, D3DXVECTOR2 size, D3DXVECTOR2 pivot, int count, float degreeAngle);
 
 void SetBackStencilBufferDraw();
 void SetBackStencilBufferMask();
@@ -20,19 +20,18 @@ void ResetBackStencilBufferDraw();
 void ResetBackStencilBufferMask();
 
 
-
-void DrawHighlight(D3DXVECTOR2 pos,float size,int count) {
+void DrawHighlight(D3DXVECTOR2 pos,D3DXVECTOR2 size,D3DXVECTOR2 pivot,int count,float startDegreeAngle) {
 	SetBackStencilBufferMask();
-
-	DrawCircle(pos, size, count);
+	DrawCircle(pos, size, pivot, count, startDegreeAngle);
 
 	ResetBackStencilBufferMask();
 }
 
-void DrawForeground(D3DXCOLOR color) {
+
+void DrawForeground(D3DCOLOR color) {
 	SetBackStencilBufferDraw();
 	SetSpriteColor(color);
-	DrawSprite(TEXTURE_INVALID_ID, {0,0 }, 100, { SCREEN_WIDTH,SCREEN_HEIGHT });
+	DrawSprite(TEXTURE_INVALID_ID, {0,0 }, -50, { SCREEN_WIDTH,SCREEN_HEIGHT });
 	SetSpriteColor(D3DCOLOR_RGBA(255,255,255,255));
 	ResetBackStencilBufferDraw();
 }
@@ -61,6 +60,16 @@ void SetBackStencilBufferMask()
 	device->SetRenderState(D3DRS_STENCILREF, 0x01);
 	device->SetRenderState(D3DRS_STENCILMASK, 0xff);
 }
+
+void ResetBackStencilBufferMask() {
+	auto device = GetD3DDevice();
+	device->SetRenderState(D3DRS_ZENABLE, g_dwCurZTest);
+	device->SetRenderState(D3DRS_ZFUNC, g_dwCurZFunc);
+
+	device->SetRenderState(D3DRS_STENCILENABLE, false);
+}
+
+
 void SetBackStencilBufferDraw()
 {
 	auto device = GetD3DDevice();
@@ -73,19 +82,15 @@ void SetBackStencilBufferDraw()
 
 }
 
-void ResetBackStencilBufferMask() {
-	auto device = GetD3DDevice();
-	device->SetRenderState(D3DRS_ZENABLE, g_dwCurZTest);
-	device->SetRenderState(D3DRS_ZFUNC, g_dwCurZFunc);
-}
-
 void ResetBackStencilBufferDraw() {
 	auto device = GetD3DDevice();
 	device->SetRenderState(D3DRS_STENCILENABLE, false);
 }
 
-void DrawCircle(D3DXVECTOR2 pos, float size, int count) {
-
+void DrawCircle(D3DXVECTOR2 pos, D3DXVECTOR2 size, D3DXVECTOR2 pivot, int count, float startDegreeAngle) {
+	if (count < 3) {
+		return;
+	}
 	auto d3dDevice = GetD3DDevice();
 
 	//このテクスチャ使ってー
@@ -102,13 +107,43 @@ void DrawCircle(D3DXVECTOR2 pos, float size, int count) {
 
 	vertexs[0].pos = D3DXVECTOR4(pos.x, pos.y, 0, 0);
 
-	for (int i = 1; i < vertexCount; i++) {
-		float rad = (i * PI * 2) / count;
-		float px = pos.x + cosf(rad) * size;
-		float py = pos.y + sinf(rad) * size;
+	float startRadAngle = startDegreeAngle * PI / 180;
 
-		vertexs[i].pos = D3DXVECTOR4(px, py, 0, 0);
+	D3DXVECTOR2 maxPos= pos;
+	D3DXVECTOR2 minPos= pos;
+
+	for (int i = 1; i < vertexCount-1; i++) {
+		float rad = ((i * PI * 2) / count)+ startRadAngle;
+		float px = pos.x + cosf(rad) * size.x;
+		float py = pos.y + sinf(rad) * size.y;
+
+		vertexs[i].pos = D3DXVECTOR4(px, py, -30, 0);
+
+		if (maxPos.x < px) {
+			maxPos.x = px;
+		}
+		if (maxPos.y < py) {
+			maxPos.y = py;
+		}
+		if (minPos.x > px) {
+			minPos.x = px;
+		}
+		if (minPos.y > py) {
+			minPos.y = py;
+		}
 	}
+	pivot.x -= 0.5;
+	pivot.y -= 0.5;
+
+	auto s = (maxPos - minPos);
+
+	for (int i = 0; i < vertexCount-1; i++) {
+		vertexs[i].pos.x -=  s.x * pivot.x;
+		vertexs[i].pos.y -=  s.y * pivot.y;
+	}
+
+	vertexs[vertexCount-1].pos = vertexs[1].pos;
+
 	d3dDevice->DrawPrimitiveUP(D3DPT_TRIANGLEFAN, count, vertexs, sizeof(Vertex2D));
 
 	delete[] vertexs;
