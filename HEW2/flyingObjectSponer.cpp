19ef45f417b03e507b937b83cd5be95ef9cf40d7
学校用 <ren02157003nao@gmail.com>
2020-12-13 16:27:41 +0900

    スポナーを治しました

--- HEW2/flyingObjectSponer.cpp
+++ HEW2/flyingObjectSponer.cpp
@@ -24,7 +24,7 @@ static int sponeMax = 0;
 //	{200,{13.5,5.5},FLYING_OBJECT_ENEMY,{-1,0}},
 //	{250,{13.5,1.5},FLYING_OBJECT_BLOCK,{-1,0}},
 //	{300,{8.5,13.5},FLYING_OBJECT_BLOCK,{0,-1}},
-//	{350,{3.5,-3.5},FLYING_OBJECT_ENEMY,{0,1}},
+//	{350,{3.5,-3.5},FLYING_OBJECT_ENEMY_BREAK_BLOCK,{0,1}},
 //	{400,{2.5,13.5},FLYING_OBJECT_BLOCK,{0,-1}},
 //	{450,{13.5,3.5},FLYING_OBJECT_BLOCK,{-1,0}},
 //	{500,{5.5,13.5},FLYING_OBJECT_BLOCK,{0,-1}},
@@ -33,14 +33,14 @@ static int sponeMax = 0;
 //	{650,{13.5,7.5},FLYING_OBJECT_BLOCK,{-1,0}},
 //	{700,{-3.5,5.5},FLYING_OBJECT_BLOCK,{1,0}},
 //	{750,{-3.5,8.5},FLYING_OBJECT_BLOCK,{1,0}},
-//	{800,{3.5,13.5},FLYING_OBJECT_ENEMY,{0,-1}},
+//	{800,{3.5,13.5},FLYING_OBJECT_ENEMY_BREAK_BLOCK,{0,-1}},
 //	{850,{13.5,5.5},FLYING_OBJECT_BLOCK,{-1,0}},
 //	{900,{2.5,13.5},FLYING_OBJECT_BLOCK,{0,-1}},
 //	{950,{8.5,-3.5},FLYING_OBJECT_ENEMY,{0,1}},
 //	{1000,{-3.5,8.5},FLYING_OBJECT_BLOCK,{1,0}},
 //	{1050,{13.5,3.5},FLYING_OBJECT_BLOCK,{-1,0}},
 //	{1100,{6.5,13.5},FLYING_OBJECT_BLOCK,{0,-1}},
-//	{1150,{4.5,-3.5},FLYING_OBJECT_ENEMY,{0,1}},
+//	{1150,{4.5,-3.5},FLYING_OBJECT_ENEMY_BREAK_BLOCK,{0,1}},
 //	{1200,{13.5,1.5},FLYING_OBJECT_BLOCK,{-1,0}},
 //	{1250,{7.5,13.5},FLYING_OBJECT_BLOCK,{0,-1}}
 //};
@@ -50,7 +50,7 @@ void InitFlyingSponer() {
 	sponeIndex = 0;
 	spones = NULL;
 
-	spones = new Spone[sponeMax];
+	//spones = new Spone[sponeMax];
 	//CopyMemory(spones, initSpone, sizeof(Spone)* sponeMax);
 
 	FlyingObjectSponerImport("stage/test/test.flyingObjectSponer");
