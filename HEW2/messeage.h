b817a0d3a8d2bebda173929f7480731252a1da88
shi <sibatamaru@gmail.com>
2021-01-13 23:48:38 +0900

    はりぼてのステージセレクトを追加

--- HEW2/messeage.h
+++ HEW2/messeage.h
@@ -13,5 +13,10 @@ void SetMessageColor(D3DCOLOR c);
 void DrawMesseage(const char* str, va_list argp);
 void DrawMesseage(const char* str, ...);
 
+void SetFontMargin(long margin);
+void SetFontRectAddY(int addY);
+void SetFontScale(D3DXVECTOR2 scale);
+void SetFontCountMax(int x, int y);
+
 //デバッグ用
 void TestMessage();
