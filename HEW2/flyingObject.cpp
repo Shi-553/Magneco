5380e5a591cf08cc3a040eb580d1191a0888fe1c
shi <sibatamaru@gmail.com>
2021-01-25 21:21:09 +0900

    アラートを付けた

--- HEW2/flyingObject.cpp
+++ HEW2/flyingObject.cpp
@@ -31,7 +31,10 @@ static int ufoBottomLightAnimationTextureId;
 static int roadSignTextureId;
 
 static int  nekoPanchiTextureId = TEXTURE_INVALID_ID;
+static int  breakAlertTextureId = TEXTURE_INVALID_ID;
+static float breakAleartFrame = 0;
 
+static bool isBreakAleartLeft = false;
 
 void BreakBlock(INTVECTOR2 pos);
 
@@ -80,9 +83,12 @@ void InitFlyingObject() {
 	roadSignTextureId = ReserveTextureLoadFile("texture/enemy/insekikuruyo.png");
 
 	nekoPanchiTextureId = ReserveTextureLoadFile("texture/block/nekopanchi.png");
+	breakAlertTextureId = ReserveTextureLoadFile("texture/enemy/breakAlert.png");
 
 	frame = 0;
+	breakAleartFrame = 0;
 	currentUID = 0;
+	isBreakAleartLeft = false;
 }
 void UninitFlyingObject() {
 	for (int i = FLYING_OBJECT_BLOCK; i < FLYING_OBJECT_MAX; i++) {
@@ -93,11 +99,26 @@ void UninitFlyingObject() {
 	ReleaseTexture(ufoBottomLightAnimationTextureId);
 	ReleaseTexture(roadSignTextureId);
 	ReleaseTexture(nekoPanchiTextureId);
+	ReleaseTexture(breakAlertTextureId);
 }
 void DrawFlyingObject(FlyingObject& flyingObject) {
 	Player* player = GetPlayer();
 	NPC* npc = GetNpc();
 	auto textureId = flyingObjectTextureIds[flyingObject.type];
+
+	if (flyingObject.type == FLYING_OBJECT_ENEMY_BREAK_BLOCK) {
+		auto pos = flyingObject.trans.pos;
+		for (int i = 0; i < 100; i++) {
+			if (GetMapType(pos)!= MAP_BLOCK) {
+				pos += flyingObject.dir;
+				continue;
+			}
+			pos = INTVECTOR2(pos).ToD3DXVECTOR2();
+			pos.x += breakAleartFrame ;
+			DrawGameSprite(breakAlertTextureId, pos, 10);
+			break;
+		}
+	}
 
 	if (flyingObject.type == FLYING_OBJECT_PURGE_BLOCK) {
 		auto tPos = D3DXVECTOR2(
@@ -195,6 +216,18 @@ void UpdateFlyingObject() {
 	}
 
 	frame++;
+	if (!isBreakAleartLeft) {
+		breakAleartFrame+=0.04;
+		if (breakAleartFrame > 0.05) {
+			isBreakAleartLeft = true;
+		}
+	}
+	else {
+		breakAleartFrame -= 0.04;
+		if (breakAleartFrame < -0.05) {
+			isBreakAleartLeft = false;
+		}
+	}
 }
 void BackFlyingObject(int frame) {
 	for (auto itr = flyingObjects.begin(); itr != flyingObjects.end();) {
@@ -213,8 +246,25 @@ bool UpdateFlyingObject(FlyingObject* flyingObject, float speed) {
 	if (flyingObject->type == FLYING_OBJECT_UFO) {
 		flyingObject->dir = (GetNpc()->trans.pos + ADD_UFO_POS) - flyingObject->trans.pos;
 
-	}
 	D3DXVec2Normalize(&flyingObject->dir, &flyingObject->dir);
+	}
+	else if (flyingObject->type == FLYING_OBJECT_PURGE_BLOCK) {
+		D3DXVec2Normalize(&flyingObject->dir, &flyingObject->dir);
+	}
+	else {
+		if (flyingObject->dir.x > 0) {
+			flyingObject->dir.x = 1;
+		}
+		if (flyingObject->dir.x < 0) {
+			flyingObject->dir.x = -1;
+		}
+		if (flyingObject->dir.y > 0) {
+			flyingObject->dir.y = 1;
+		}
+		if (flyingObject->dir.y < 0) {
+			flyingObject->dir.y = -1;
+		}
+	}
 	flyingObject->trans.pos += flyingObject->dir * speed * GetDeltaTime();
 
 	flyingObject->trans.UpdatePos();
