6d59739f40b7ef0d82ca52968c4a1411c597ae92
shi <sibatamaru@gmail.com>
2021-01-29 11:50:55 +0900

    スタッフロールの最後に押してっていうの追加

--- マグネッコ/staffRole.cpp
+++ マグネッコ/staffRole.cpp
@@ -8,12 +8,15 @@
 #include "InputLogger.h"
 #include "sound.h"
 
-#define STAFF_ROLETITLE_WIDTH 640
-#define STAFF_ROLETITLE_HEIGHT 240
+#define STAFF_ROLE_TITLE_WIDTH 640
+#define STAFF_ROLE_TITLE_HEIGHT 240
+
+#define STAFF_ROLE_PRESS_O_WIDTH 128
+#define STAFF_ROLE_PRESS_O_HEIGHT 32
 
 static Message* staffRoleMessage;
 static float frame = 0;
-static std::string staffRole = 
+static std::string staffRole =
 R"(
 スタッフロール
 
@@ -84,19 +87,24 @@ On-Jin ～音人～ https://on-jin.com/
 
 
 
+
 Thank you for playing!!
 )";
-static int staffRoleKaigyoCount = std::count(staffRole.cbegin(), staffRole.cend(), '\n')+2;
+static int staffRoleKaigyoCount = std::count(staffRole.cbegin(), staffRole.cend(), '\n') + 2;
 static int oneSizeHeight;
 static int staffRoleId = TEXTURE_INVALID_ID;
 static int staffRoleTitleId = TEXTURE_INVALID_ID;
+static int pressOTexture = TEXTURE_INVALID_ID;
+static int animeFrame = 0;
 
 void InitStaffRole() {
 	staffRoleMessage = new Message();
 	frame = 0;
+	animeFrame = 0;
 	oneSizeHeight = staffRoleMessage->GetOneSize().y;
 	staffRoleId = ReserveTextureLoadFile("texture/background/背景１.png");
 	staffRoleTitleId = ReserveTextureLoadFile("texture/ui/title_640×240.png");
+	pressOTexture = ReserveTextureLoadFile("texture/ui/press_o_128×32.png");
 	PlaySound(SOUND_LABEL_BGM004);
 }
 void UninitStaffRole() {
@@ -104,9 +112,11 @@ void UninitStaffRole() {
 	delete staffRoleMessage;
 	ReleaseTexture(staffRoleId);
 	ReleaseTexture(staffRoleTitleId);
+	ReleaseTexture(pressOTexture);
 }
 void UpdateStaffRole() {
-	if (-SCREEN_HEIGHT +frame > staffRoleKaigyoCount * oneSizeHeight-(SCREEN_HEIGHT/2)-200) {
+	if (-SCREEN_HEIGHT + frame > staffRoleKaigyoCount * oneSizeHeight - (SCREEN_HEIGHT / 2) - 200) {
+		animeFrame++;
 		if (TriggerInputLogger(MYVK_ENTER)) {
 			GoNextScene(GameStartScene);
 		}
@@ -120,7 +130,7 @@ void UpdateStaffRole() {
 }
 void DrawStaffRole() {
 	SetSpriteColor(D3DCOLOR_RGBA(100, 100, 100, 255));
-	DrawSprite(staffRoleId, D3DXVECTOR2(0, 0), 10, D3DXVECTOR2(SCREEN_WIDTH,SCREEN_HEIGHT));
+	DrawSprite(staffRoleId, D3DXVECTOR2(0, 0), 10, D3DXVECTOR2(SCREEN_WIDTH, SCREEN_HEIGHT));
 	SetSpriteColor(D3DCOLOR_RGBA(255, 255, 255, 255));
 
 	staffRoleMessage->ClearOffset();
@@ -130,5 +140,9 @@ void DrawStaffRole() {
 	staffRoleMessage->Draw(staffRole.c_str());
 
 
-	DrawSprite(staffRoleTitleId, D3DXVECTOR2(SCREEN_WIDTH/2- STAFF_ROLETITLE_WIDTH/2,(staffRoleKaigyoCount * oneSizeHeight)+300 - frame), 10);
+	DrawSprite(staffRoleTitleId, D3DXVECTOR2(SCREEN_WIDTH / 2 - STAFF_ROLE_TITLE_WIDTH / 2, (staffRoleKaigyoCount * oneSizeHeight) + 300 - frame), 10);
+	if (animeFrame >= 96) {
+		DrawSprite(pressOTexture, { 990,590 }, 10, D3DXVECTOR2(STAFF_ROLE_PRESS_O_WIDTH, STAFF_ROLE_PRESS_O_HEIGHT),
+				D3DXVECTOR2(0, 32 * (((int)(animeFrame / 8.0)+7) % 8)), D3DXVECTOR2(STAFF_ROLE_PRESS_O_WIDTH, STAFF_ROLE_PRESS_O_HEIGHT));
+	}
 }
