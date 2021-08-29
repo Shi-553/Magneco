9d470252ea2c4625ab0f8a3701b6e153162e3911
Shi-553 <58556078+Shi-553@users.noreply.github.com>
2021-08-29 17:24:52 +0900

    直接入力の文字化け修正

--- マグネッコ/ImGuiRenderer.cpp
+++ マグネッコ/ImGuiRenderer.cpp
@@ -4,9 +4,14 @@
 #include "imgui/imgui_impl_dx9.h"
 #include "imgui/imgui_impl_win32.h"
 #include "myd3d.h"
+#include <string>
+#include <strsafe.h>
+#include "StringConvert.hpp"
 
 extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
+bool isDBCS = false;
+BYTE befByte;
 
 void ImGuiRenderer::Init(HWND hWnd) {
 
@@ -22,6 +27,7 @@ void ImGuiRenderer::Init(HWND hWnd) {
 
 	io.Fonts->AddFontFromFileTTF("C:\\Windows\\Fonts\\meiryo.ttc", 18.0f, nullptr, io.Fonts->GetGlyphRangesJapanese());
 
+	isDBCS = false;
 }
 void ImGuiRenderer::Uninit() {
 	ImGui_ImplDX9_Shutdown();
@@ -29,7 +35,34 @@ void ImGuiRenderer::Uninit() {
 	ImGui::DestroyContext();
 
 }
+
+
 LRESULT CALLBACK ImGuiRenderer::Proc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+
+	if (msg == WM_CHAR) {
+		BYTE c = (BYTE)wParam;
+
+		if (c > 128|| isDBCS)
+		{
+			if (!isDBCS) {
+				isDBCS = true;
+				befByte = c;
+				return false;
+			}
+			else {
+				short s = (befByte | c << 8);//２つの文字をあわせて漢字に変換
+				char ch[sizeof(short) + 1];
+				ZeroMemory(ch, sizeof(ch));
+				StringCchCat(ch, sizeof(ch), (const char*)&s);
+
+
+				auto wstr = StringConvert::MultiToWide(std::string(ch));
+				wParam = wstr[0];
+			}
+		}
+
+	}
+	isDBCS = false;
 	return ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam);
 }
 void ImGuiRenderer::DrawBegin() {
